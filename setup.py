dcad1d1612060f06f82c9df2061cca51d63a076a
1602saurab <saurabhsonibrd@gmail.com>
2024-09-23 11:39:51 +0530

    all_packages_updated

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requiremet(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+print(get_requiremet("./requirements.txt"))
+
+__version__ = "0.0.4"
+REPO_NAME = "mongodbconnectorpkg"
+PKG_NAME= "Mongo-Connect"
+AUTHOR_USER_NAME = "1602saurab"
+AUTHOR_EMAIL = "saurabhsonibrd@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    #long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+     install_requires=get_requiremet("requirements_dev.txt") 
+    )
